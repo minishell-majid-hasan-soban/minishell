7473615bdeda06add1395649f7db44783c00dc93
amajid <amajid@student.1337.ma>
2024-02-03 18:42:36 +0100

    adding sume tokenazation functionality, warrning its not yet complete

--- parser/main.c
+++ parser/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "pars.h"
+#include "structs.h"
 
 void *ft_realloc(void *ptr, size_t old_size, size_t new_size) {
     if (new_size == 0 && ptr != NULL)
@@ -20,11 +21,75 @@ void *ft_realloc(void *ptr, size_t old_size, size_t new_size) {
    return new_ptr;
 }
 
+
+int	is_space(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\f' || c == '\r' || c == '\v')
+		return (1);
+	return (0);
+}
+
+void skip_space(char **prompt)
+{
+	while(is_space(**prompt))
+		((*prompt)++);
+}
+
+int add_separator(t_token_type type, char **prompt, t_token_arr *tokens)
+{
+	if(tokens->count + 1 > tokens->size)
+	{
+		ft_realloc(tokens->arr, tokens->size, tokens->size * 2);
+		if(!tokens->arr)
+			return -1;
+		tokens->size *= 2;
+	}
+	tokens->arr[tokens->count] = (t_token){NULL, type, NULL};
+	(*prompt)++;
+	if (type == TOKEN_DLESS || type == TOKEN_DGREAT || type == TOKEN_OR || type == TOKEN_AND)
+		(*prompt)++;
+	return 1;
+}
+
+int handle_seperator(char **prompt, t_token_arr *tokens)
+{
+	if (!ft_strncmp(*prompt, "<<", 2))
+		return (add_separator(TOKEN_DLESS, prompt, tokens) && 1);
+	else if (!ft_strncmp(*prompt, ">>", 2))
+		return (add_separator(TOKEN_DGREAT, prompt, tokens) && 1);
+	else if (!ft_strncmp(*prompt, "<", 1))
+		return (add_separator(TOKEN_LESS, prompt, tokens) && 1);
+	else if (!ft_strncmp(*prompt, ">", 1))
+		return (add_separator(TOKEN_GREAT, prompt, tokens) && 1);
+	else if (!ft_strncmp(*prompt, "&&", 2))
+		return (add_separator(TOKEN_AND, prompt, tokens) && 1);
+	else if (!ft_strncmp(*prompt, "||", 2))
+		return (add_separator(TOKEN_OR, prompt, tokens) && 1);
+	else
+		return (add_separator(TOKEN_PIPE, prompt, tokens) && 1);
+}
+
 t_token_arr tokenize(char *prompt)
 {
 	t_token_arr res;
+	int			ret;
 
-	res = ft_realloc(res.arr, res.size, size_t new_size)
+	res = (t_token_arr){0, 32, 0};
+	res.arr = ft_realloc(res.arr, res.size, res.size);
+
+	while(*prompt)
+	{
+		if(is_space(*prompt))
+			skip_space(&prompt);
+		if(!ft_strncmp(prompt, "<", 1) || !ft_strncmp(prompt, "|", 1)
+			|| !ft_strncmp(prompt, ">", 1) || !ft_strncmp(prompt, "&&", 2))
+			ret = handle_seperator(&prompt, &res);
+		else
+		 ;
+	}
+
+	
 }
 
 
--- parser/pars.h
+++ parser/pars.h
@@ -4,32 +4,147 @@
 #include "../libft/libft.h"
 
 
-#define TOKEN_command 0
-#define TOKEN_argument 1
-#define TOKEN_pipe 2
-#define TOKEN_red_out 3
-#define TOKEN_red_in 4
-#define TOKEN_here_doc_in 5
-#define TOKEN_here_doc_out 6
-#define TOKEN_env_var 7
-#define TOKEN_exit_status 8
+/*
+** t_token_type: enum for token types:
+** TOKEN_WORD: word meaning a command or an argument or a file name
+*/
+typedef enum	e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_GREAT,
+	TOKEN_LESS,
+	TOKEN_DGREAT,
+	TOKEN_DLESS,
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_NEWLINE,
+	TOKEN_EOF
+}				t_token_type;
 
+/*
+** t_token_error: enum for token errors:
+** T_ERROR_NONE: no error
+** T_SYNTAX: syntax error like > at the end of the line, Example: echo >
+*/
+typedef enum t_token_error
+{
+	T_ERROR_NONE,
+	T_SYNTAX,
+	T_MALLOC
+	
+}				t_token_error;
 
-
-
-
-
-
-typedef struct s_token {
-    int		type;   // TOKEN_COMMAND or TOKEN_ARGUMENT
-    char	*value;
-} t_token;
-
+/*
+** t_token: struct for tokens:
+** value: the value of the token if it's a word, like "echo" or "file" or "argument"
+** type: the type of the token
+** error: the error of the token, if TOKEN_NEWLINE is followed by nothing, it's a syntax error
+** next: the next token, list easy a sat
+** prev: the previous token
+*/
+typedef struct	s_token
+{
+	char				*value;
+	int					type;
+	t_token_error		*error;
+}						t_token;
 
 typedef struct s_token_arr
 {
 	t_token	*arr;
 	long	size;
-} t_token_arr;
+	long	count;
+}	t_token_arr;
+
+/*
+** t_node_type: enum for node types: node of the tree (ast)
+** NODE_COMMAND: a command node can be a simple command or a command with arguments + redirections
+** NODE_PIPE: a pipe node, a node with a pipe token
+** NODE_AND: a node with an and token
+** NODE_OR: a node with an or token
+*/
+typedef enum e_node_type
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+	NODE_AND,
+	NODE_OR
+}				t_node_type;
+
+/*
+** t_redirection_type: enum for redirection types:
+** R_INPUT: input redirection, like "echo < file"
+** R_OUTPUT: output redirection, like "echo > file"
+** R_APPEND: append redirection, like "echo >> file"
+** R_HEREDOC: heredoc redirection, like "echo << file"
+*/
+typedef enum e_redirection_type
+{
+	R_INPUT,
+	R_OUTPUT,
+	R_APPEND,
+	R_HEREDOC
+}				t_redirection_type;
+
+/*
+** t_redirection: struct for redirections:
+** type: the type of the redirection, like R_INPUT or R_OUTPUT [t_redirection_type enum above]
+** file: the file of the redirection, like "file" in "echo hello > file"
+** heredoc_fd: used if the redirection is a heredoc, it hodls the read end of the pipe
+** next: the next redirection
+** prev: the previous redirection
+*/
+typedef struct	s_redirection
+{
+	t_redirection_type		type;
+	char					*file;
+	int						heredoc_fd;
+}							t_redirection;
+
+/*
+** t_command: struct for commands:
+** name: the name of the command, like "echo" or "ls"
+** args: the arguments of the command, like "echo hello" -> "hello" is the argument
+** redirections: the redirections of the command, like "echo hello > file" -> "file" is the redirection of type R_OUTPUT
+** t_redirection: is above this struct
+*/
+typedef struct	s_command
+{
+	char			*name;
+	char			**args;
+	t_redirection	*redirections;
+	long			redirection_size;
+}				t_command;
+
+/*
+** t_node: struct for the tree nodes:
+** token: the token of the node
+** left: the left child of the node
+** right: the right child of the node
+*/
+typedef struct	s_node
+{
+	t_node_type			token;
+	t_command			command;
+	struct s_node		*children;
+}				t_node;
+
+typedef struct s_ast_tree
+{
+	t_node tree;
+} t_ast_tree;
+
+/*
+** t_ast: struct for the abstract syntax tree:
+** graphical representation of the tree: cmd1 a > b > c | cmd2 d < e
+** 		      PIPE
+** 		     /      \
+** 		 COMMAND    COMMAND
+** 		 /  |  \    /  |  \
+** 		a > b > c   d  <   e
+** 		root: the root of the tree
+*/
+
 
 #endif
