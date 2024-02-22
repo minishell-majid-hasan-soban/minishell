4b5a263384a9fadd487004fd45c57c76e4272ae5
amajid <abdelilahmajid012@gmail.com>
2024-02-22 21:52:17 +0100

    trying to fix the "(cmd) redir del cmd2" syntax error include heredoc execution

--- parser/add_seperator.c
+++ parser/add_seperator.c
@@ -6,7 +6,7 @@
 /*   By: amajid <amajid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 15:58:17 by amajid            #+#    #+#             */
-/*   Updated: 2024/02/22 16:36:30 by amajid           ###   ########.fr       */
+/*   Updated: 2024/02/22 21:47:59 by amajid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,14 +48,17 @@ int	check_errors_tokens(t_token_arr *tokens, t_ast *ast)
 	t_token_type	type;
 	long			index;
 	int				is_in_op;
+	long			j;
+	bool			is_word_found;
+	bool			is_after_parantheses;
 
 	is_in_op = 0;
 	index = 0;
+	is_after_parantheses = false;
 	while (index < tokens->count)
 	{
 		past_type = tokens->arr[index - 1 * (index > 0)].type;
 		type = tokens->arr[index].type;
-		print_token(&tokens->arr[index]);
 		if (is_in_op <= 0 && type == TOKEN_CP)
 		{
 			print_parse_error_near(&tokens->arr[index]);
@@ -65,6 +68,10 @@ int	check_errors_tokens(t_token_arr *tokens, t_ast *ast)
 			is_in_op++;
 		if (type == TOKEN_CP)
 			is_in_op--;
+		if (type == TOKEN_CP && is_in_op == 0)
+			is_after_parantheses = true;
+		if (type == TOKEN_PIPE || type == TOKEN_AND || type == TOKEN_OR)
+			is_after_parantheses = false;
 		if ((type == TOKEN_AND || type == TOKEN_OR
 				|| type == TOKEN_PIPE) && index == 0)
 		{
@@ -79,14 +86,46 @@ int	check_errors_tokens(t_token_arr *tokens, t_ast *ast)
 			print_parse_error_near(&tokens->arr[index]);
 			return (-1);
 		}
-		if ((index - 1 * (index > 0) != index) && type != TOKEN_WORD
+		if (((index - 1 * (index > 0)) != index) && type != TOKEN_WORD
 			&& (past_type == TOKEN_DGREAT
 				|| past_type == TOKEN_DLESS || past_type == TOKEN_GREAT
-				|| past_type == TOKEN_LESS))
+				|| past_type == TOKEN_LESS) && !is_after_parantheses)
 		{
 			print_parse_error_near(&tokens->arr[index]);
 			return (-1);
 		}
+		print_token(&tokens->arr[index]);
+		if ((type == TOKEN_DGREAT || type == TOKEN_DLESS
+				|| type == TOKEN_GREAT || type == TOKEN_LESS)
+			&& is_after_parantheses)
+		{
+			j = index;
+			is_word_found = false;
+			while (tokens->arr[j].type != TOKEN_EOF)
+			{
+				j++;
+				print_token(&tokens->arr[j]);
+				if (tokens->arr[j - 1].type == TOKEN_DLESS
+					&& tokens->arr[j].type == TOKEN_WORD)
+					tokens->arr[j - 1].heredoc_fd
+						= init_here_doc(ast, tokens->arr[j].value);
+				if (is_word_found && tokens->arr[j].type == TOKEN_WORD)
+				{
+					print_parse_error_near(&tokens->arr[j]);
+					return (-1);
+				}
+				else if (is_word_found && (tokens->arr[j].type == TOKEN_AND
+						|| tokens->arr[j].type == TOKEN_OR
+						|| tokens->arr[j].type == TOKEN_PIPE))
+					break ;
+				else if (is_word_found)
+					is_word_found = false;
+				if (!is_word_found && tokens->arr[j].type == TOKEN_WORD)
+					is_word_found = true;
+			}
+			index = j;
+		}
+		printf("----\n");
 		if (((type == TOKEN_OP && past_type != TOKEN_OP)
 				&& (past_type != TOKEN_AND && past_type != TOKEN_OR
 					&& past_type != TOKEN_PIPE))
