20278e0f927467e97ed8f97bd04d120dc70c1424
Hassan Sobane <hsobane@e1r10p12.1337.ma>
2024-02-23 19:14:16 +0100

    working on unset

--- execution/builtins_cmd/unset.c
+++ execution/builtins_cmd/unset.c
@@ -6,7 +6,7 @@
 /*   By: hsobane <hsobane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 03:23:59 by hsobane           #+#    #+#             */
-/*   Updated: 2024/02/16 07:53:10 by hsobane          ###   ########.fr       */
+/*   Updated: 2024/02/23 18:30:42 by hsobane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,22 @@ void	ft_unsetenv(char *name, t_shell *shell)
 int	ft_unset(t_ast *ast, char **args)
 {
 	int	i;
-
+	int	ret;
+	
+	ret = 0;
 	i = 1;
 	while (args[i])
 	{
-		ft_unsetenv(args[i], ast->shell);
+		if (!ft_valid_name(args[i]))
+		{
+			ft_putstr_fd("minishell: unset: `", 2);
+			ft_putstr_fd(args[i], 2);
+			ft_putstr_fd("': not a valid identifier\n", 2);
+			ret = 1;
+		}
+		else
+			ft_unsetenv(args[i], ast->shell);
 		i++;
 	}
-	return (0);
+	return (ret);
 }
