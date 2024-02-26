06e1c40b77c35e14744e0ec6088d177fd3f3481d
Hassan Sobane <hsobane@e1r10p11.1337.ma>
2024-02-26 14:33:07 +0100

    norm

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: hsobane <hsobane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 10:27:17 by hsobane           #+#    #+#             */
-/*   Updated: 2024/02/26 14:21:44 by hsobane          ###   ########.fr       */
+/*   Updated: 2024/02/26 14:28:40 by hsobane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,59 +30,14 @@ int	exit_status(int newstatus, bool set)
 	return (status);
 }
 
-static void reset_termios(struct termios og_term)
-{
-	tcsetattr(STDIN_FILENO, TCSANOW, &og_term);
-}
-
-int	ft_readline(t_shell *shell)
-{
-	shell->line = readline(GREEN"minishell> "RESET);
-	if (shell->line == NULL)
-		(reset_termios(shell->term), ft_putstr_fd("exit\n", 1),
-		ft_free_shell(shell));
-	if (ft_strlen(shell->line) == 0)
-	{
-		free(shell->line);
-		return (1);
-	}
-	add_history(shell->line);
-	return (0);
-}
-
-t_token_arr	ft_get_token(t_shell *shell)
-{
-	t_token_arr	tokens;
-	t_ast		token_ast;
-	int			her_status;
-
-	token_ast.shell = shell;
-	tokens = tokenize(shell->line);
-	her_status = check_errors_tokens(&tokens, &token_ast);
-	if (her_status == -1 || her_status == 130)
-	{
-		if (her_status == -1)
-			shell->exit_status = 2;
-		else
-			shell->exit_status = 1;
-		free_token_arr(&tokens);
-		tokens = (t_token_arr){0};
-	}
-	return (tokens);
-}
-
 int main(int argc, char **argv, char **envp)
 {
-	t_shell		shell;
-	t_ast		*ast;
-	t_token_arr tokens;
-	t_ast		token_ast;
-	int			her_status;
+	t_shell			shell;
+	t_token_arr 	tokens;
 	
 	(void)argc;
 	(void)argv;
-	token_ast.shell = &shell;
-	ft_init_shell(&shell, envp, &token_ast);
+	ft_init_shell(&shell, envp);
 	while (1)
 	{
 		if (ft_readline(&shell))
@@ -93,9 +48,8 @@ int main(int argc, char **argv, char **envp)
 		free_token_arr(&tokens);
 		run_cmd(&shell);
 		ast_running(false, true);
-		ast = NULL;
 		if (isatty(0) == 0)
-			break ;
+			return (ft_free_shell(&shell), 0);
 		free(shell.line);
 		(dup2(shell.fd_in, 0), dup2(shell.fd_out, 1));
 	}
