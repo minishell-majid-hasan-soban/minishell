7389381fb2c2e331817ff63e1e3b80dd7e271038
amajid <abdelilahmajid012@gmail.com>
2024-02-28 18:07:17 +0100

    norm

--- parser/part_6.c
+++ parser/part_6.c
@@ -6,7 +6,7 @@
 /*   By: amajid <amajid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 21:59:34 by amajid            #+#    #+#             */
-/*   Updated: 2024/02/27 18:29:23 by amajid           ###   ########.fr       */
+/*   Updated: 2024/02/28 18:01:55 by amajid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,65 @@ t_redirection_type	get_redirection_type(t_token_type type)
 	return (R_HEREDOC);
 }
 
+t_ast	*extract_command_part1(t_token **curr_token, t_ast *ast)
+{
+	t_redirection	*redir;
+
+	while ((*curr_token)->type != TOKEN_AND && (*curr_token)->type != TOKEN_OR
+		&& (*curr_token)->type != TOKEN_PIPE && (*curr_token)->type != TOKEN_EOF
+		&& (*curr_token)->type != TOKEN_OP && (*curr_token)->type != TOKEN_CP)
+	{
+		if ((*curr_token)->type != TOKEN_WORD)
+		{
+			redir = create_redirection((*curr_token), *((*curr_token) + 1));
+			if (redir == NULL)
+				return (ft_putstr_fd(ALLOC_ERR, 2), free_ast(ast), (NULL));
+			add_back_redirection(&ast->redirections, redir);
+			(*curr_token)++;
+		}
+		else
+		{
+			print_parse_error_near((*curr_token));
+			free_ast(ast);
+			return (NULL);
+		}
+		(*curr_token)++;
+	}
+	return (ast);
+}
+
+int	extract_command_part2(t_token **curr_token, t_command *command)
+{
+	t_redirection	*redir;
+
+	while ((*curr_token)->type != TOKEN_AND && (*curr_token)->type != TOKEN_OR
+		&& (*curr_token)->type != TOKEN_PIPE && (*curr_token)->type != TOKEN_EOF
+		&& (*curr_token)->type != TOKEN_OP && (*curr_token)->type != TOKEN_CP)
+	{
+		if ((*curr_token)->type == TOKEN_WORD)
+			add_node_arg(command, (*curr_token)->value);
+		else
+		{
+			redir = create_redirection((*curr_token), *((*curr_token) + 1));
+			if (redir == NULL)
+			{
+				free_command(command);
+				return (-1);
+			}
+			add_back_redirection(&command->redirections, redir);
+			(*curr_token)++;
+		}
+		(*curr_token)++;
+	}
+	return (1);
+}
+
+
+
 t_ast	*extract_command(t_token **curr_token)
 {
 	t_command		*command;
 	t_ast			*ast;
-	t_redirection	*redir;
 
 	command = ft_calloc(1, sizeof(t_command));
 	if (!command)
@@ -47,47 +101,10 @@ t_ast	*extract_command(t_token **curr_token)
 			return (NULL);
 		}
 		(*curr_token)++;
-		while ((*curr_token)->type != TOKEN_AND && (*curr_token)->type != TOKEN_OR
-			&& (*curr_token)->type != TOKEN_PIPE && (*curr_token)->type != TOKEN_EOF
-			&& (*curr_token)->type != TOKEN_OP && (*curr_token)->type != TOKEN_CP)
-		{
-			if ((*curr_token)->type != TOKEN_WORD)
-			{
-				redir = create_redirection((*curr_token), *((*curr_token) + 1));
-				if (redir == NULL)
-					return (ft_putstr_fd(ALLOC_ERR, 2), free_ast(ast), (NULL));
-				add_back_redirection(&ast->redirections, redir);
-				(*curr_token)++;
-			}
-			else
-			{
-				print_parse_error_near((*curr_token));
-				free_ast(ast);
-				return (NULL);
-			}
-			(*curr_token)++;
-		}
-		return (ast);
+		return (extract_command_part1(curr_token, ast));
 	}
-	while ((*curr_token)->type != TOKEN_AND && (*curr_token)->type != TOKEN_OR
-		&& (*curr_token)->type != TOKEN_PIPE && (*curr_token)->type != TOKEN_EOF
-		&& (*curr_token)->type != TOKEN_OP && (*curr_token)->type != TOKEN_CP)
-	{
-		if ((*curr_token)->type == TOKEN_WORD)
-			add_node_arg(command, (*curr_token)->value);
-		else
-		{
-			redir = create_redirection((*curr_token), *((*curr_token) + 1));
-			if (redir == NULL)
-			{
-				free_command(command);
-				return (NULL);
-			}
-			add_back_redirection(&command->redirections, redir);
-			(*curr_token)++;
-		}
-		(*curr_token)++;
-	}
+	if (extract_command_part2(curr_token, command) == -1)
+		return (NULL);
 	return (create_ast_node(N_CMD, command));
 }
 
